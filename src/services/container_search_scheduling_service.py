4871ea085a5a7cef14adf2e924db854d92936116
PabloQuadros <pablo.aquadros@gmail.com>
2025-05-21 00:56:15 -0300

    fix container search schedule

--- src/services/container_search_scheduling_service.py
+++ src/services/container_search_scheduling_service.py
@@ -24,13 +24,13 @@ class ContainerSearchSchedulerService:
         self.container_mapper = container_mapper
 
     async def start(self):
-        while True:
-            now = datetime.now()
-            #minute=0,
-            next_hour = (now + timedelta(hours=1)).replace(minute=0, second=0, microsecond=0)
-            wait_seconds = (next_hour - now).total_seconds()
-            print(f"[{now}] Aguardando {int(wait_seconds)} segundos até a próxima hora cheia...")
-            await asyncio.sleep(wait_seconds)
+        #while True:
+            # now = datetime.now()
+            # #minute=0,
+            # next_hour = (now + timedelta(hours=1)).replace(minute=0, second=0, microsecond=0)
+            # wait_seconds = (next_hour - now).total_seconds()
+            # print(f"[{now}] Aguardando {int(wait_seconds)} segundos até a próxima hora cheia...")
+            # await asyncio.sleep(wait_seconds)
             try:
                 await self.execute_search_routine()
             except Exception as e:
@@ -48,28 +48,31 @@ class ContainerSearchSchedulerService:
             if not scheduling or not scheduling.containers:
                 print("Nenhum agendamento encontrado para esta hora.")
                 return
+            # containers_to_search = [
+            #     cs for cs in scheduling.containers
+            #     if start.time() <= cs.search_time <= end.time()
+            # ]
             containers_to_search = [
                 cs for cs in scheduling.containers
-                if start.time() <= cs.search_time <= end.time()
+                if datetime.combine(datetime.today(), cs.search_time) >= now
             ]
             containers_to_search.sort(key=lambda c: c.search_time)
 
             for container in containers_to_search:
-                await self._search_single_container(container)
+                scheduled_time = datetime.combine(datetime.today(), container.search_time)
+                now = datetime.now()
+                wait_time = (scheduled_time - now).total_seconds()
+
+                if wait_time > 0:
+                    print(f"Aguardando {int(wait_time)}s para buscar container {container.container_number}")
+                    await asyncio.sleep(wait_time)
+                await self.search_single_container(container)
 
         except Exception as e:
             print(f"[{datetime.now()}] Erro inesperado ao executar rotina de busca: {e}")
 
-    async def _search_single_container(self, container):
+    async def search_single_container(self, container):
         try:
-            scheduled_time = datetime.combine(datetime.today(), container.search_time)
-            now = datetime.now()
-            wait_time = (scheduled_time - now).total_seconds()
-
-            if wait_time > 0:
-                print(f"Aguardando {int(wait_time)}s para buscar container {container.container_number}")
-                await asyncio.sleep(wait_time)
-
             print(f"[{datetime.now().time()}] Executando busca para {container.container_number}")
             msc_response = await self.msc_service.get_tracking_info(container.container_number)
             actual_container = await self.container_repository.get_by_number(container.container_number)
@@ -92,6 +95,9 @@ class ContainerSearchSchedulerService:
                 print("Nenhuma mudança detectada nas informações do contêiner.")
         except Exception as e:
             print(f"[{datetime.now()}] Erro ao buscar container {container.container_number}: {e}")
+            if actual_container:
+                actual_container.add_search_log(SearchStatus.FAILURE)
+                await self.container_repository.update(actual_container)
 
             
                 
